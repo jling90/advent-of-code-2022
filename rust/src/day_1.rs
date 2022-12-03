174d951c3ecfe3e23929cc7de5d1bdcef82ca06b
Jun Ling <jun@insight.co>
2022-12-04 09:52:34 +1100

    Add boilerplate

---
+++ rust/src/day_1.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{self};
+
+/*
+ * https://adventofcode.com/2021/day/1
+ *
+ * Read through a file, check if last line (as int)
+ * is larger than the previous value and return number
+ * of times there was an increase.
+ */
+
+pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> String {
+    "stubbed".to_string()
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+mod day_1;
+mod util;
+
+const DAY_1_FILE_PATH: &str = "./resources/day_1";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day_num = &args[1];
+    let task_num = &args[2];
+
+    match day_num.as_str() {
+        "1" => match task_num.as_str() {
+            "1" => {
+                if let Ok(lines) = util::read_lines(DAY_1_FILE_PATH) {
+                    println!("{}", day_1::task_one(lines));
+                } else {
+                    println!("Failed to read file");
+                }
+            }
+            _ => println!("Task not implemented"),
+        },
+        _ => println!("No match for exercise {}, task {}", day_num, task_num),
+    }
 }
