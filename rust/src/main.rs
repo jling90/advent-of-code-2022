c7913e3dbfd3ba1703333e167c7a25c7edd5e641
Jun Ling <jun@insight.co>
2022-12-04 17:14:54 +1100

    Chore: make task running logic terser

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,41 +3,34 @@ mod day_1;
 mod day_2;
 mod util;
 
-const DAY_1_FILE_PATH: &str = "./resources/day_1";
-const DAY_2_FILE_PATH: &str = "./resources/day_2";
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let day_num = &args[1];
     let task_num = &args[2];
 
-    match day_num.as_str() {
-        "1" => match task_num.as_str() {
-            "1" => {
-                if let Ok(lines) = util::read_lines(DAY_1_FILE_PATH) {
-                    println!("{}", day_1::task_one(lines));
-                } else {
-                    println!("Failed to read file");
-                }
+    let filename = format!("./resources/day_{}", day_num);
+
+    match format!("{} {}", day_num, task_num).as_str() {
+        "1 1" => {
+            if let Ok(lines) = util::read_lines(filename) {
+                println!("{}", day_1::task_one(lines));
+            } else {
+                println!("Failed to read file");
             }
-            "2" => {
-                if let Ok(lines) = util::read_lines(DAY_1_FILE_PATH) {
-                    println!("{}", day_1::task_two(lines));
-                } else {
-                    println!("Failed to read file");
-                }
+        }
+        "1 2" => {
+            if let Ok(lines) = util::read_lines(filename) {
+                println!("{}", day_1::task_two(lines));
+            } else {
+                println!("Failed to read file");
             }
-            _ => println!("Task not implemented"),
-        },
-        "2" => match task_num.as_str() {
-            "1" => {
-                println!(
-                    "{}",
-                    day_2::task_one(util::read_lines_to_string_vec(DAY_2_FILE_PATH))
-                )
-            }
-            _ => println!("Task not implemented"),
-        },
+        }
+        "2 1" => {
+            println!(
+                "{}",
+                day_2::task_one(util::read_lines_to_string_vec(filename))
+            )
+        }
         _ => println!("No match for exercise {}, task {}", day_num, task_num),
     }
 }
