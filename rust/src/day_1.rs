42c5b02f3304a18d09bc48923f46a80bbf0460d4
Jun Ling <jun@insight.co>
2022-12-04 11:42:59 +1100

    Solve day 1-1

--- rust/src/day_1.rs
+++ rust/src/day_1.rs
@@ -1,14 +1,27 @@
+use std::cmp;
 use std::fs::File;
 use std::io::{self};
 
 /*
- * https://adventofcode.com/2021/day/1
+ * https://adventofcode.com/2022/day/1
  *
- * Read through a file, check if last line (as int)
- * is larger than the previous value and return number
- * of times there was an increase.
+ * Read through a file, sum contiguous rows of ints
+ * and return the max sum.
  */
 
 pub fn task_one(lines: io::Lines<io::BufReader<File>>) -> String {
-    "stubbed".to_string()
+    let mut sum = 0;
+    let mut max_sum = 0;
+    for line in lines {
+        if let Ok(parsed) = line {
+            if let Ok(n) = parsed.parse::<i32>() {
+                sum = n + sum;
+            } else {
+                max_sum = cmp::max(max_sum, sum);
+                sum = 0;
+            }
+        }
+    }
+
+    max_sum.to_string()
 }
